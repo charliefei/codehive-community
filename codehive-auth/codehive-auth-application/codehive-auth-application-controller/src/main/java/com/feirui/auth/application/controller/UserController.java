efeef78c66a8d59b355ef70bd2724c64f76cb8ba
charliefei <2062753446@qq.com>
2024-01-18 20:32:28 +0800

    鉴权v1: 鉴权模块搭建完毕

---
+++ codehive-auth/codehive-auth-application/codehive-auth-application-controller/src/main/java/com/feirui/auth/application/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.feirui.auth.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
