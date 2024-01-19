ce9bf8e7c57133530df6488da9828f7c24392217
charliefei <2062753446@qq.com>
2024-01-19 13:39:13 +0800

    feat: subject/auth/gateway/oss注册完毕

--- codehive-gateway/src/main/java/com/feirui/gateway/GatewayApplication.java
+++ codehive-gateway/src/main/java/com/feirui/gateway/GatewayApplication.java
@@ -1,18 +1,13 @@
-package com.feirui.subject;
+package com.feirui.gateway;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-/**
- * 刷题服务入口启动类
- */
 @SpringBootApplication
 @ComponentScan("com.feirui")
-@MapperScan("com.feirui.**.dao")
-public class SubjectApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SubjectApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
