71610aac71f2c1dc00e1214a348d9423e99a720a
charliefei <2062753446@qq.com>
2025-01-14 16:12:18 +0800

    build: 练题服务搭建完毕

---
+++ codehive-practice/codehive-practice-server/src/main/java/com/feirui/practice/server/PracticeApplication.java
@@ -0,0 +1,17 @@
+package com.feirui.practice.server;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.feirui")
+@MapperScan("com.feirui.**.dao")
+public class PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeApplication.class, args);
+    }
+
+}
