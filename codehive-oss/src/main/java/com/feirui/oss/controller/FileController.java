7836d33933753391f722c8314fa8ab6814b2675d
charliefei <2062753446@qq.com>
2025-02-24 19:31:56 +0800

    bugfix: 修复一些问题

--- codehive-oss/src/main/java/com/feirui/oss/controller/FileController.java
+++ codehive-oss/src/main/java/com/feirui/oss/controller/FileController.java
@@ -3,6 +3,7 @@ package com.feirui.oss.controller;
 import com.feirui.oss.entity.Result;
 import com.feirui.oss.service.FileService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class FileController {
     @Resource
     private FileService fileService;
