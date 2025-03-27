862d0ae330adbf80b7e7d16dc763bd2cd291ddba
charliefei <2062753446@qq.com>
2025-03-27 12:26:37 +0800

    feat: ai服务增强

--- codehive-ai/src/main/java/com/feirui/ai/controller/AiChatController.java
+++ codehive-ai/src/main/java/com/feirui/ai/controller/AiChatController.java
@@ -1,17 +1,16 @@
 package com.feirui.ai.controller;
 
+import cn.hutool.json.JSONUtil;
 import com.feirui.ai.domain.ChatRequest;
 import com.feirui.ai.service.DeepSeekService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.codec.ServerSentEvent;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import javax.annotation.Resource;
 import java.util.Collections;
+import java.util.Map;
 
 import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
 
@@ -41,6 +40,17 @@ public class AiChatController {
         ChatRequest question = ChatRequest.builder()
                 .model("deepseek-chat")
                 .messages(Collections.singletonList(new ChatRequest.Message("user", query)))
+                .stream(true)
+                .build();
+        return deepSeekService.generateResponseAsStreamV2(question);
+    }
+
+    @PostMapping(value = "/chat/stream/v3", produces = TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<String>> chatV3(@RequestBody Map<String, Object> map) {
+        log.info("map: {}", JSONUtil.toJsonStr(map));
+        ChatRequest question = ChatRequest.builder()
+                .model("deepseek-chat")
+                .messages(Collections.singletonList(new ChatRequest.Message("user", String.valueOf(map.get("query")))))
                 .stream(true)
                 .build();
         return deepSeekService.generateResponseAsStreamV2(question);
